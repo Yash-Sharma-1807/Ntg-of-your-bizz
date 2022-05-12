562c693863649c18afd68260527160ccc682cde6
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-05-12 21:53:20 +0530

    Update __init__.py

--- vexana/__init__.py
+++ vexana/__init__.py
@@ -47,161 +47,73 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
         "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
     )
     quit(1)
+TOKEN = "5369769829:AAH84a7Wdtth8XaZKGHhOnXDwU731yV53eE"
 
-ENV = bool(os.environ.get("ENV", False))
-
-if ENV:
-    TOKEN = os.environ.get("TOKEN", None)
-
-    try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", None))
-    except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
-
-    JOIN_LOGGER = os.environ.get("JOIN_LOGGER", None)
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
-
-    try:
-        DRAGONS = {int(x) for x in os.environ.get("DRAGONS", "").split()}
-        DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
-
-    try:
-        DEMONS = {int(x) for x in os.environ.get("DEMONS", "").split()}
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WOLVES = {int(x) for x in os.environ.get("WOLVES", "").split()}
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    try:
-        TIGERS = {int(x) for x in os.environ.get("TIGERS", "").split()}
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
-
-    INFOPIC = bool(os.environ.get("INFOPIC", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", "-1001580719176")
-    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
-    URL = os.environ.get("URL", "")  # Does not contain token
-    PORT = int(os.environ.get("PORT", 5000))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vexana_Robot")
-    BOT_NAME = os.environ.get("BOT_NAME", "Vexana_Robot")
-    CERT_PATH = os.environ.get("CERT_PATH")
-    API_ID = os.environ.get("API_ID", None)
-    API_HASH = os.environ.get("API_HASH", None)
-    BOT_ID = int(os.environ.get("BOT_ID", None))
-    DB_URI = os.environ.get("DATABASE_URL")
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    DONATION_LINK = os.environ.get("DONATION_LINK")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", "e8c43576833a8867d24a4e6785349e20")
-    VIRUS_API_KEY = os.environ.get("VIRUS_API_KEY", None)
-    LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
-    WORKERS = int(os.environ.get("WORKERS", 8))
-    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", "V7NS1NBFEL4X24L6")
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", "2AS711XS1O9B")
-    AI_API_KEY = os.environ.get("AI_API_KEY", None)
-    WALL_API = os.environ.get("WALL_API", None)
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
-    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
-    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-    ARQ_API_KEY = os.environ.get('ARQ_API_KEY', None)
-    GBAN_LOGS = -1001744920557
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-
-    try:
-        BL_CHATS = {int(x) for x in os.environ.get("BL_CHATS", "").split()}
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
-
-else:
-    from vexana.config import Development as Config
-
-    TOKEN = Config.TOKEN
-
-    try:
+try:
         OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
+except ValueError:
         raise Exception("Your OWNER_ID variable is not a valid integer.")
 
-    JOIN_LOGGER = Config.JOIN_LOGGER
-    OWNER_USERNAME = Config.OWNER_USERNAME
-    ALLOW_CHATS = Config.ALLOW_CHATS
-    try:
-        DRAGONS = {int(x) for x in Config.DRAGONS or []}
-        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
-    except ValueError:
+JOIN_LOGGER = "-1532103402"
+OWNER_USERNAME = "DEBOJYOTI_IZ_OP"
+try:
+        DRAGONS = []
+        DEV_USERS = [2069126270]
+except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
-    try:
-        DEMONS = {int(x) for x in Config.DEMONS or []}
-    except ValueError:
+try:
+        DEMONS = [2069126270]
+except ValueError:
         raise Exception("Your support users list does not contain valid integers.")
 
-    try:
-        WOLVES = {int(x) for x in Config.WOLVES or []}
-    except ValueError:
+try:
+        WOLVES = []
+except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
-    try:
-        TIGERS = {int(x) for x in Config.TIGERS or []}
-    except ValueError:
+try:
+        TIGERS = []
+except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
 
-    EVENT_LOGS = Config.EVENT_LOGS
-    WEBHOOK = Config.WEBHOOK
-    URL = Config.URL
-    PORT = Config.PORT
-    CERT_PATH = Config.CERT_PATH
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-    ARQ_API_URL = Config.ARQ_API_URL
+EVENT_LOGS = "-1532103402"
+WEBHOOK = False
+URL = None
+PORT = 5000
+CERT_PATH = None
+API_ID = 12771791
+API_HASH = "50af18f49c357d4e53b74c4b8d38b59e"
+ARQ_API_URL = "RVLOWH-KMDHXW-QQCPAC-EFICND-ARQ"
 
-    DB_URI = Config.SQLALCHEMY_DATABASE_URI
-    MONGO_DB_URI = Config.MONGO_DB_URI
-    HEROKU_API_KEY = Config.HEROKU_API_KEY
-    HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
-    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
-    BOT_ID = Config.BOT_ID
-    VIRUS_API_KEY = Config.VIRUS_API_KEY
-    DONATION_LINK = Config.DONATION_LINK
-    LOAD = Config.LOAD
-    NO_LOAD = Config.NO_LOAD
-    DEL_CMDS = Config.DEL_CMDS
-    STRICT_GBAN = Config.STRICT_GBAN
-    WORKERS = Config.WORKERS
-    BAN_STICKER = Config.BAN_STICKER
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    TIME_API_KEY = Config.TIME_API_KEY
-    AI_API_KEY = Config.AI_API_KEY
-    WALL_API = Config.WALL_API
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
-    SPAMWATCH_API = Config.SPAMWATCH_API
-    INFOPIC = Config.INFOPIC
-    REDIS_URL = Config.REDIS_URL
+DB_URI = "postgresql://postgres:rnOdrDgO0Mhrm6AxwN7q@containers-us-west-58.railway.app:7505/railway"
+MONGO_DB_URI = "mongodb://mongo:8RR2A3CfpRsy2hqXipnS@containers-us-west-58.railway.app:7602"
+TEMP_DOWNLOAD_DIRECTORY = "/tmp"
+OPENWEATHERMAP_ID = ""
+BOT_ID = 5369769829
+VIRUS_API_KEY = ""
+DONATION_LINK = ""
+LOAD = []
+NO_LOAD = []
+DEL_CMDS = True
+STRICT_GBAN = True
+WORKERS = (8)
+BAN_STICKER = ""
+ALLOW_EXCL = True
+CASH_API_KEY = ""
+TIME_API_KEY = ""
+AI_API_KEY = ""
+WALL_API = ""
+SUPPORT_CHAT = "OLiviasupportbot"
+SPAMWATCH_SUPPORT_CHAT = "OLiviasupportbot"
+SPAMWATCH_API = ""
+INFOPIC = True
+REDIS_URL = "redis://default:53twL1GHGn4eceqj5I4i@containers-us-west-60.railway.app:5988"
 
-    try:
-        BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
-    except ValueError:
+try:
+        BL_CHATS = []
+except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
-
-DRAGONS.add(OWNER_ID)
-DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(5001573230)
-DEV_USERS.add(5001573230)
 
 if not SPAMWATCH_API:
     sw = None
